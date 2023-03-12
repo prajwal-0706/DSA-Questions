e4237dc665f58bb621c6c642a2e891013428ab11
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-03-13 01:09:29 +0530

    Update

--- Patterns/advanced_patterns.cpp
+++ Patterns/advanced_patterns.cpp
@@ -3,17 +3,15 @@ using namespace std;
 
 int main(){
     int n;
-    cout << "Enter the Value of 'n' :-";
+    cout << "Enter the Value of 'n' :- ";
     cin >> n;
 
     for(int i = 1 ; i <= n ; i++){
 
-        //spaces
         for(int j = n - i ; j >= 1 ; j--){
             cout << " ";
         }
 
-        //stars
         for(int j = 1 ; j <= i ; j++){
             cout << "*" << " ";
         }
