ccda01294faf0ec3cb666b4058f11545aef88a22
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-06-15 21:00:32 +0530

    Update

---
+++ Main Codes/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a[5];
+  for (int i = 0; i < 5; i++)
+  {
+    cin >> a[i];
+  }
+
+  for (int i = 0; i <= 10; i++)
+  {
+    cout << a[i];
+  }
+}
